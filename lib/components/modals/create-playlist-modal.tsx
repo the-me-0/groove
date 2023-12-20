989b496f7e8a0452e235360b1e16978f7ffba3e7
the-me-0 <119881173+the-me-0@users.noreply.github.com>
2023-12-20 11:19:17 -0500

    [ADD] Playlist creation modal - unfinished
    Added zod & shadcn's form to the project, file upload seems to work - not tested.
    No error in console, unfinished : songs selection not done yet

--- lib/components/modals/create-playlist-modal.tsx
+++ lib/components/modals/create-playlist-modal.tsx
@@ -9,77 +9,107 @@ import {
   DialogTitle
 } from '@/lib/shadcn-components/ui/dialog';
 import { useModal } from '@/hooks/use-modal-store';
-import {FieldValues, SubmitHandler, useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {useState} from "react";
-import Input from "@/lib/components/input";
+import {Input} from "@/lib/shadcn-components/ui/input";
 import Button from "@/lib/components/Button";
 import toast from "react-hot-toast";
 import { useUser } from "@/hooks/use-user";
 import {useRouter} from "next/navigation";
 import SearchInput from "@/lib/components/SearchInput";
 import {Song} from "@prisma/client";
+import axios from "axios";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/lib/shadcn-components/ui/form";
 
 export const CreatePlaylistModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
   const router = useRouter();
   const userContext = useUser();
-  const [isLoading, setIsLoading] = useState(false);
-
   const [songs, setSongs] = useState<Song[]>([]);
-
-  const { onOpen, isOpen, onClose, type, data } = useModal();
-  const { register, handleSubmit, reset } = useForm<FieldValues>({
-    defaultValues: {
-      title: '',
-      image: null
-    }
-  });
 
   const isModalOpen = isOpen && type === 'create-playlist';
 
-  const onChange = () => {
-    reset();
-    onClose();
-  }
+  // Image rules
+  const MAX_IMAGE_SIZE = 5; // 5 MB
+  const ALLOWED_IMAGE_TYPES = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/jpg",
+  ];
 
-  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+  const formSchema = z.object({
+    name: z.string().min(1, {
+      message: 'Playlist name is required.'
+    }),
+    image: z.custom<File>()
+      .refine(
+        (file) => file !== undefined, {
+          message: 'Please provide an image.'
+        }
+      )
+      .refine(
+        (file) => !file || (!!file && file.size <= MAX_IMAGE_SIZE*1048576), // *1024*1024
+        {
+          message: `The playlist picture must be a maximum of ${MAX_IMAGE_SIZE}MB.`,
+        }
+      )
+      .refine(
+        (file) => ALLOWED_IMAGE_TYPES.includes(file?.type),
+        "Only these types are allowed .jpg, .jpeg, .png and .webp"
+      ),
+    songsId: z.array(z.string()) // no refine because we handle this one directly in this component
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  })
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof  formSchema>) => {
     try {
-      setIsLoading(true);
-
-      const imageFile = values.image?.[0];
-
-      if (!imageFile || !userContext.user) {
-        toast.error('Missing fields');
+      if (!userContext.user) {
+        toast.error("Couldn't find user");
         return;
       }
 
-      const formData = new FormData();
-      formData.append('title', values.title);
-      formData.append('image', imageFile);
-      formData.append('songs', songs.map((song) => song.id));
+      if (!songs.length) {
+        toast.error("Please select at least one song.");
+        return;
+      }
 
-      const response = await fetch('/api/song', {
-        method: 'POST',
-        body: formData,
-      });
+      if (!form.getValues().image) {
+        toast.error("Please select a background image !");
+        return;
+      }
+
+      const response = await axios.post('/api/playlists', values);
 
       if (response.status !== 200) {
         return toast.error('Song upload failed.');
       }
 
+      toast.success('Playlist created !');
+
+      form.reset();
       router.refresh();
-      toast.success('Song created !');
-      reset();
       onClose();
     } catch (error) {
       toast.error('Something went wrong');
       console.log(error);
-    } finally {
-      setIsLoading(false);
     }
   }
 
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  }
+
   return (
-    <Dialog open={isModalOpen} onOpenChange={onChange}>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
       <DialogContent className='bg-neutral-900 text-white overflow-hidden'>
         <DialogHeader className='pt-8 px-6'>
           <DialogTitle className='text-2xl text-center font-bold'>
@@ -89,42 +119,88 @@ export const CreatePlaylistModal = () => {
             Name it & add songs
           </DialogDescription>
         </DialogHeader>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='flex flex-col gap-y-4'
-        >
-          <div className="space-y-8">
-            <Input
-              id='title'
-              disabled={isLoading}
-              {...register('title', {required: true})}
-              placeholder='Song title'
-            />
-            <div>
-              <div className='pb-1'>
-                Select an image
-              </div>
-              <Input
-                id='image'
-                type='file'
-                disabled={isLoading}
-                accept='image/*'
-                {...register('image', {required: true})}
-              />
-            </div>
-            <div className='w-full p-2 rounded-lg bg-neutral-800'>
-              <SearchInput inputPlaceholder={'Search songs to add...'} />
-            </div>
-            <div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='flex flex-col gap-y-4'
+          >
+            <div className="space-y-8">
+              {/* ~~~~~~~~~~~~ IMAGE UPLOAD ~~~~~~~~~~~~ */}
+              <div>
+                <div className='pb-1'>
+                  Select an image
+                </div>
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field: { onChange }, ...field }) => {
+                    // Passing fieldState and formState to the Input seem to provoke errors in console
+                    const { fieldState, formState, ...someField } = field;
 
+                    return (
+                      <FormItem>
+                        <FormLabel>Image</FormLabel>
+                        {/* File Upload */}
+                        <FormControl>
+                          <Input
+                            type="file"
+                            accept="image/*"
+                            disabled={form.formState.isSubmitting}
+                            { ...someField }
+                            onChange={(event) => onChange(event.target.files ? event.target.files[0] : null)}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+
+              {/* ~~~~~~~~~~~~ NAME ~~~~~~~~~~~~ */}
+              <FormField
+                control={form.control}
+                name={'name'}
+                render={({ field: { onChange }, ...field }) => {
+                  // Passing fieldState and formState to the Input seem to provoke errors in console
+                  const {fieldState, formState, ...someField} = field;
+
+                  return (
+                    <FormItem>
+                      <FormLabel>
+                        Playlist name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          placeholder={'Enter playlist name'}
+                          {...someField}
+                          onChange={onChange}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+
+              {/* ~~~~~~~~~~~~ SONGS SELECTION ~~~~~~~~~~~~ */}
+              <div className='w-full p-2 rounded-lg bg-neutral-800'>
+                {/* ModalSearchInput -> onOpen('xxxxx', data) */}
+                <SearchInput inputPlaceholder={'Search songs to add...'}/>
+              </div>
+              <div>
+
+              </div>
+              {/* ~~~~~~~~~~~~ SUBMIT ~~~~~~~~~~~~ */}
+              <DialogFooter>
+                <Button disabled={isLoading} type='submit'>
+                  Create
+                </Button>
+              </DialogFooter>
             </div>
-            <DialogFooter>
-              <Button disabled={isLoading} type='submit'>
-                Create
-              </Button>
-            </DialogFooter>
-          </div>
-        </form>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
