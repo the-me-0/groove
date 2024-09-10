6256f88057dc2b3d2e18cd55ccdce65db0ea006f
the-me-0 <119881173+the-me-0@users.noreply.github.com>
2024-09-10 11:43:56 +0200

    Test with progress bar concept

--- lib/components/Test.tsx
+++ lib/components/Test.tsx
@@ -1,120 +1,47 @@
 'use client';
 
-import React, {useEffect, useState} from 'react';
-import usePlayer from '@/hooks/player/use-player';
-import useClock from '@/hooks/player/use-clock';
+import React, {useEffect, useRef, useState} from 'react';
 
-interface TestProps {
-  audioPlayer: HTMLAudioElement,
-}
-
-const samples = 150;
-
-export const Test: React.FC<TestProps> = ({
-  audioPlayer,
-}) => {
-  const player = usePlayer();
-  const clock = useClock();
-
-  const [filteredAudioBuffer, setFilteredAudioBuffer] = useState<Array<number> | null>(null);
+export const Test = () => {
+  const [progress, setProgress] = useState(20);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!filteredAudioBuffer || !audioPlayer || isNaN(audioPlayer.duration)) return;
+    const scrollContainer = scrollContainerRef.current;
 
-    console.log('audioPlayer.currentTime', audioPlayer.currentTime, ', duration', audioPlayer.duration);
+    if (scrollContainer) {
+      const handleScroll = (event: WheelEvent) => {
+        event.preventDefault(); // Empêche le défilement de la page
+        const delta = event.deltaY;
 
-    /**
-     * Draws the audio file into a canvas element.
-     * @param {Array} normalizedData The filtered array returned from filterData()
-     * @returns {Array} a normalized array of data
-     */
-    const draw = (normalizedData: Array<number>): void => {
-      // set up the canvas
-      const canvas = document.querySelector("canvas");
-      if (!canvas) {
-        console.error('Canvas not found');
-        return;
-      }
-      const dpr = window.devicePixelRatio || 1;
-      const padding = 75;
-      canvas.width = canvas.offsetWidth * dpr;
-      canvas.height = (canvas.offsetHeight + padding * 2) * dpr;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) {
-        console.error('Canvas context not found');
-        return;
-      }
-      // clear the current state
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+        setProgress((prevValue) => {
+          if (delta > 0) {
+            return Math.min(prevValue + 1, 100); // Scroll vers le bas diminue la valeur
+          } else {
+            return Math.max(prevValue - 1, 0); // Scroll vers le haut augmente la valeur
+          }
+        });
+      };
 
-      ctx.scale(dpr, dpr);
-      ctx.translate(0, canvas.offsetHeight / 2 + padding); // set Y = 0 to be in the middle of the canvas
+      // Ajout de l'écouteur d'événement avec `passive: false`
+      scrollContainer.addEventListener('wheel', handleScroll, { passive: false });
 
-      const shouldBeColoredUpto = Math.floor(audioPlayer.currentTime / audioPlayer.duration * normalizedData.length);
-
-      // draw the line segments
-      const width = canvas.offsetWidth / normalizedData.length;
-      let index = 0;
-
-      const drawLine = () => {
-        const x = width * index;
-        let height = normalizedData[index] * canvas.offsetHeight - padding;
-        if (height < 0) {
-          height = 0;
-        } else if (height > canvas.offsetHeight / 2) {
-          height = height - canvas.offsetHeight / 2;
-        }
-
-        drawLineSegment(ctx, x, height, width, Boolean((index + 1) % 2), index < shouldBeColoredUpto);
-
-        if (index < normalizedData.length) {
-          index++;
-          requestAnimationFrame(drawLine);
-        }
-      }
-
-      drawLine();
-
-      // for (let i = 0; i < normalizedData.length; i++) {
-      //   const x = width * i;
-      //   let height = normalizedData[i] * canvas.offsetHeight - padding;
-      //   if (height < 0) {
-      //     height = 0;
-      //   } else if (height > canvas.offsetHeight / 2) {
-      //     height = height - canvas.offsetHeight / 2;
-      //   }
-      //
-      //   drawLineSegment(ctx, x, height, width, Boolean((i + 1) % 2), i < shouldBeColoredUpto);
-      // }
-    };
-
-    /**
-     * A utility function for drawing our line segments
-     * @param {AudioContext} ctx the audio context
-     * @param {number} x  the x coordinate of the beginning of the line segment
-     * @param {number} height the desired height of the line segment
-     * @param {number} width the desired width of the line segment
-     * @param {boolean} isEven whether or not the segmented is even-numbered
-     */
-    const drawLineSegment = (ctx: CanvasRenderingContext2D, x: number, height: number, width: number, isEven: boolean, isColored) => {
-      ctx.lineWidth = 1; // how thick the line is
-      // ctx.strokeStyle = "#fff"; // what color our line is
-      ctx.strokeStyle = isColored ? '#cb4f00' : '#fff';
-      ctx.beginPath();
-      height = isEven ? height : -height;
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, height);
-      ctx.arc(x + width / 2, height, width / 2, Math.PI, 0, isEven);
-      ctx.lineTo(x + width, 0);
-      ctx.stroke();
-    };
-
-    draw(filteredAudioBuffer);
-  }, [filteredAudioBuffer, clock, audioPlayer]);
+      // Nettoyage lors du démontage du composant
+      return () => {
+        scrollContainer.removeEventListener('wheel', handleScroll);
+      };
+    }
+  }, []);
 
   return (
-    <div className='hidden sm:flex w-80 xl:w-full max-w-xl mx-auto'>
-      <canvas className='h-[80px] w-full'></canvas>
+    <div
+      className='absolute top-1/2 left-1/2 flex-col gap-4 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-black flex justify-center items-center overflow-hidden'>
+      scroll on the progress radius...
+
+      {/* @ts-ignore */}
+      <div ref={scrollContainerRef} className='radial-progress' style={{"--value": progress}} role='progressbar'>
+        {progress}%
+      </div>
     </div>
-  )
+  );
 }
